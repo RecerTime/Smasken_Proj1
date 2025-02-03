447981cdaa7070ee6a7703e7c1e9a985e03b5f0c
jakobButBetter <dahlgren.birger.jakob@gmail.com>
2025-02-03 12:43:02 +0100

    Wow mer grejer

--- Preprocessing.py
+++ Preprocessing.py
@@ -31,13 +31,25 @@ scalar = skl_pre.StandardScaler()
 
 
 def Preprocess(df):
+    df_lenght = len(df)
     # Turn hour of day, day of week and month into sin terms to reflect cyclical nature
     df["hour_of_day"] = np.sin(2 * np.pi * df["hour_of_day"] / 24)
     df["day_of_week"] = np.sin(2 * np.pi * df["day_of_week"] / 7)
     df["month"] = np.sin(2 * np.pi * df["month"] / 12)
-    # Make a rush hour flag
-    # Make a workday flag which is all days where it is weekday and not holiday
-    df["workday"] = np.zeros(len(df))
+    # Make a night flag
+    # workday flag which is all days where it is weekday and not holiday
+    df["workday"] = np.zeros(df_lenght)
     df["workday"] = (df["weekday"] == 1) & (df["holiday"] == 0)
-    # Normalize all non binary values using either min max or Z dont know which
+    # Flag for rush hour
+    df["rush"] = np.zeros(df_lenght)
+    df["rush"] = (
+        np.sin(2 * np.pi * 6 / 24) <= df["hour_of_day"] <= np.sin(2 * np.pi * 9 / 24)
+        or (
+            np.sin(2 * np.pi * 16 / 24)
+            <= df["hour_of_day"]
+            <= np.sin(2 * np.pi * 19 / 24)
+        )
+    ) and df["workday"] == 1
+    # Make a bad weather flag
+    # Normalize all non binary values using either min max or Z dont know which :(
     return df
